048a1688e06350191047c952fbc7c009e212f8e1
Konstantin0 <maschkovc@yandex.ru>
2022-03-02 21:24:27 +0300

    Добавление логики для управления драйвера(очистка кук, закрытие драйвера)

--- src/main/java/common/Config.java
+++ src/main/java/common/Config.java
@@ -10,4 +10,14 @@ public class Config {
 
     public static final String PLATFORM_AND_BROWSER = "CHROME_WINDOWS";
 
+    /**
+     * Очищает файлы cookies
+     */
+    public static final Boolean CLEAR_COOKIES = true;
+
+    /**
+     * Закрывает браузер
+     */
+    public static final Boolean HOLD_BROWSER_OPEN = true;
+
 }
